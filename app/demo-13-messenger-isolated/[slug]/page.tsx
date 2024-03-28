de89b816191b82bba4be71fe37be95ba12c43757
Sam Selikoff <sam.selikoff@gmail.com>
2024-03-28 15:59:01 -0400

    wip

--- app/demo-13-messenger-isolated/[slug]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default async function Page({ params }: { params: { slug: string } }) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return <p>Page {params.slug}</p>;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ProgressBar, ProgressBarLink } from "./progress-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,18 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-800 text-gray-200 antialiased`}
       >
-        {children}
+        <ProgressBar className="fixed h-1 shadow-lg shadow-sky-500/20 bg-sky-500 top-0">
+          <header className="border-b border-gray-700">
+            <nav className="m-4 flex gap-4">
+              <ProgressBarLink href="/">Home</ProgressBarLink>
+              <ProgressBarLink href="/posts/1">Post 1</ProgressBarLink>
+              <ProgressBarLink href="/posts/2">Post 2</ProgressBarLink>
+              <ProgressBarLink href="/posts/3">Post 3</ProgressBarLink>
+            </nav>
+          </header>
+
+          <div className="m-4">{children}</div>
+        </ProgressBar>
       </body>
     </html>
   );
